f619da268b2414ae1d4dd7eda6b8197e421d5294
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-13 13:08:50 +0800

    product list

--- madlibscontinue.py
+++ madlibscontinue.py
@@ -311,3 +311,35 @@ while index < len(count_list):
   index = index + 1
 # Congratulations! You just created a distribution table of a list of numbers! 
 # This is also known as a histogram
+
+
+#==========================================================
+
+# Define a procedure, product_list,
+# that takes as input a list of numbers,
+# and returns a number that is
+# the result of multiplying all
+# those numbers together.
+
+def product_list(list_of_numbers):
+        index = 0
+        product = 1
+        while index < len(list_of_numbers):
+                product *= list_of_numbers[index]
+                index+=1
+
+        return product
+
+
+
+
+
+
+print product_list([9])
+#>>> 9
+
+print product_list([1,2,3,4])
+#>>> 24
+
+#print product_list([])
+#>>> 1
